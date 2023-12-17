4051ee94f137bcb6c8bb3334bb273ed666158488
Zeamech <simonjamesburgess1257@gmail.com>
2023-12-17 10:10:36 +0000

    Added Main Menu

--- Assets/Scripts/Main Menu.cs
+++ Assets/Scripts/Main Menu.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject HowToPlayScene;
+
+    public void Awake()
+    {
+        HowToPlayScene.SetActive(false);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(sceneName: "Scene");
@@ -13,5 +21,15 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void HowToPlay()
+    {
+        HowToPlayScene.SetActive(true);
+    }
+
+    public void Back()
+    {
+        HowToPlayScene.SetActive(false);
     }
 }
